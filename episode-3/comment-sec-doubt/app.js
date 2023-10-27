9ef849a6a42a5e11b12360d7e85918dbff2b1602
Puneet <lpuneetnj@gmail.com>
2023-10-28 02:37:48 +0530

    Worked upon a doubt

---
+++ episode-3/comment-sec-doubt/app.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const h1 = <h1>This is heading</h1>;
+
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+
+root.render(h1);
