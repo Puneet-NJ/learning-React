b3d9900f50712dc6f5a3e93f209a18902c787898
Puneet NJ <lpuneetnj@gmail.com>
2024-11-18 19:41:23 +0530

    practice

---
+++ Intro-practice/dist/index.c4775257.js
@@ -0,0 +1,40 @@
+/**
+ * <div parent1>
+ *      <div>
+ *          <h1>hi from h1 tag</h1>
+ *      </div>
+ *
+ *      <div>
+ *          <h1>hi from h1 tag</h1>
+ *      </div>
+ * </div>
+ */ const parent = React.createElement("div", {
+    id: "parent"
+}, [
+    React.createElement("div", {
+        id: "child"
+    }, [
+        React.createElement("h1", {
+            id: "child"
+        }, "hello from h1"),
+        React.createElement("h2", {
+            id: "child"
+        }, "hello from h2")
+    ]),
+    ,
+    React.createElement("div", {
+        id: "child2"
+    }, [
+        React.createElement("h1", {
+            id: "child"
+        }, "hello from h1"),
+        React.createElement("h2", {
+            id: "child"
+        }, "hello from h2")
+    ])
+]);
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
+//# sourceMappingURL=index.c4775257.js.map
